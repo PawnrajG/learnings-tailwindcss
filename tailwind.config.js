6790c50d6d1d793a7132bcf8df20702053ed25cf
webDev-Learner <pawnmugi@gmail.com>
2024-05-01 01:04:14 +0530

    first commit
    This site is developed to learn tailwindcss and npm package manager

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html',"./build/js/*.js"], 
+  theme: {
+    extend: {
+      Keyframes: {
+        'open-menu': {
+          '0%' :{ transform: 'scaleY(0)' },
+          '80%' :{ transform: 'scaleY(1.2)' },
+          '100%' :{ transform: 'scaleY(1)' },
+        },
+      },
+
+      animation: {
+        'open-menu' : 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
+
